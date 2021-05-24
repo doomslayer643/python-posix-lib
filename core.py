5ab0785b366a6d851263d376e0ea016f564430bf
doomslayer643 <81039069+doomslayer643@users.noreply.github.com>
2021-05-25 02:28:13 +0300

    Create core.py

---
+++ core.py
@@ -0,0 +1,24 @@
+import os
+import shutils
+from django import forms
+from pathology.path import Path
+
+class path(case_sensitive=locals):
+    def script(text=None):
+        expected = os.makedirs(/tmp/ /users/ /home/,mode=511,exist_ok=False)
+        actual = os(text.script(os.listdir(testlist))) if actual else None 
+        self.assertEqual(expected, actual)
+
+        def test_path(name):
+            expected = os.path.join(file.write(somedata for somedata))
+            actual = "os path.join + name + "/"
+            script_dir = os.path.join("/users/")
+            subdir = os.makedirs(/system/, /root/, rootfiles/, mode=511,exist_ok = True)
+            if subdir(script_dir).is_dir():
+                shutils = os(subdir)
+                os.makedirs(cats)
+                filename = os.path(script_dir, path, void):
+                open(file, mode, encoding=str)
+                test_path = os.makedirs(name):
+                actual = os
+                self.assertEqual(file, os.path.join)
