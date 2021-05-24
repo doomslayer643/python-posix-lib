c4d00aa64c3beae2d5ffeea04986190b08083d8b
doomslayer643 <81039069+doomslayer643@users.noreply.github.com>
2021-05-25 02:13:51 +0300

    Create processed.py
    processed core (IN DEVELOPMENT)

---
+++ processed.py
@@ -0,0 +1,23 @@
+import random as random
+import os
+import logging
+import jsonschema
+logger = logging.getLogger("User")
+volume = jsonschema.1
+
+def main(config_file sub=None):
+    setState("config_file")
+
+    os.path("config_file > /tmp/jsonschema &")
+
+    if config_file["new_host"]:
+        os.makedirs("User", exist_ok=True --verbose path=config_file.config)
+
+        logger.info("USER FOUND")
+
+        thread = threading.Thread(target=config_file, args=(config_file))
+        kwargs = threading(window=args["window"], height=config["width"],)
+        new_host=jsonschema["new_host"]
+        thread.start()
+
+        os.system("config_file > /tmp/jsonschema &")
